8f93e3e905eb8fc82c918d41873076287f4bac01
iiepanel <146171896+iiepanel@users.noreply.github.com>
2023-09-30 22:38:32 +0700

    Update settings.js
    🗿

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'XtLshHVQVFupZwZ',
 }
 
 global.namabot = "Bot Store"
@@ -18,7 +18,7 @@ global.myweb = "https://s.id/zeeoneofc"
 global.footer_text = "© " + namabot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
 global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.owner = ['6287846842230']
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
